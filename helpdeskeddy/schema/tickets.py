492165fb1fd5711df90ff0706627bef853d03fc6
shanart <shanin.x@gmail.com>
2021-03-12 01:23:34 +0200

    add ticket object

--- helpdeskeddy/schema/tickets.py
+++ helpdeskeddy/schema/tickets.py
@@ -33,3 +33,84 @@ class GetTicketsUrlParamsSchema(Schema):
         available_directions = ['asc', 'desc']
         if value:
             raise ValidationError("Wrong order_by param")
+
+
+class TicketObject(Schema):
+    # число (необязательный) Пример: 23
+    # ID родительской заявки
+    pid = fields.Int()
+
+    # строка (обязательный) Пример: Problem with email
+    # Название заявки
+    title = fields.String()
+
+    # строка (обязательный) Пример: <p>Problem with email info@example.com</p>
+    # Текст заявки
+    description = fields.String()
+
+    # дата (необязательный) Пример: 01.01.1970 00:00
+    # Дата и время SLA
+    sla_date = fields.DateTime(format='%Y-%m-%d %H:%M:%S')
+
+    # дата (необязательный) Пример: 01.01.1970 00:00
+    # Заморозка заявки до даты и времени
+    freeze_date = fields.DateTime(format='%Y-%m-%d %H:%M:%S')
+
+    # строка (необязательный) Пример: process
+    # Статус заявки
+    status_id = fields.String()
+
+    # число (необязательный) Пример: 1
+    # ID приоритета
+    priority_id = fields.String()
+
+    # число (необязательный) Пример: 2
+    # ID типа
+    type_id = fields.String()
+
+    # число (необязательный) Пример: 1
+    # ID департамента
+    department_id = fields.String()
+
+    # булево (необязательный) Пример: false
+    # Блокировка заявки. true - блокировать заявку. false - разблокировать заявку.
+    ticket_lock = fields.String()
+
+    # число (необязательный) Пример: 1
+    # ID исполнителя
+    owner_id = fields.String()
+
+    # число (необязательный) Пример: 2
+    # ID владельца заявки
+    user_id = fields.String()
+
+    # число (необязательный) Пример: email@example.com
+    # Если не задан ID владельца заявки, то для создания заявки будет использоваться э-почта пользователя, если пользователь не существует, то он будет автоматически создан
+    user_email = fields.String()
+
+    # массив почтовых ящиков (необязательный) 
+    # Копия письма
+    cc = fields.String()
+
+    # массив почтовых ящиков (необязательный) 
+    # Скрытая копия письма
+    bcc = fields.String()
+
+    # массив user_id (необязательный) 
+    # Список пользователей следящих за заявкой, пользователь должен быть сотрудником.
+    followers = fields.String()
+
+    # число (необязательный) Пример: 1
+    # Первый ответ в заявке от имени сотрудника – 0, от имени пользователя 1, по умолчанию 1
+    create_from_user = fields.String()
+
+    # массив (необязательный) Пример: [1 => 3, 2 => 0]
+    # Массив индивидуальных полей заявки, пример: custom_fields[field_id] = value, в случае иерархического поля, необходимо указывать уровень custom_fields[field_id][level] = value
+    custom_fields = fields.String()
+
+    # массив файлов (необязательный) 
+    # Массив приложений
+    files = fields.String()
+
+
+    
